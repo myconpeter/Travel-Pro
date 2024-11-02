ac7289e9dd9c31aaa757f0c1778680d7b2d13272
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2024-11-02 18:50:42 +0100

    okk

--- models/createShipSchema.js
+++ models/createShipSchema.js
@@ -1,31 +1,34 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
 
-const createShipSchema = mongoose.Schema({
-    name:String,
-    orderNumber:String,
-    gate:String,
-    shipmentNo:String,
-    shipmentType:String,
-    time:String,
-    from:String,
-    currentLocation:String,
-    to:String,
-    departure:String,
-    ticketId: String,
-    client:String,
-    item1:String,
-    item2:String,
-    item3:String,
-    item4:String,
-    item5:String,
-    item6:String,
-    item7:String,
-    item8:String,
-    item9:String,
-    item10:String,
+const createShipSchema = mongoose.Schema(
+	{
+		name: String,
+		orderNumber: String,
+		gate: String,
+		shipmentNo: String,
+		shipmentType: String,
+		time: String,
+		from: String,
+		dueDate: String,
+		currentLocation: String,
+		to: String,
+		departure: String,
+		ticketId: String,
+		client: String,
+		item1: String,
+		item2: String,
+		item3: String,
+		item4: String,
+		item5: String,
+		item6: String,
+		item7: String,
+		item8: String,
+		item9: String,
+		item10: String,
+	},
+	{ timestamps: true }
+);
 
-}, { timestamps: true })
-
-const Ship = mongoose.model('Ship', createShipSchema)
+const Ship = mongoose.model('Ship', createShipSchema);
 
 module.exports = Ship;
